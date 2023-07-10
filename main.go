ebee2b13343eb3f1e5e26992024270a1ba0ffa98
Adam Bibby <abibby@mero.co>
2023-07-10 13:34:39 -0400

    fail without cache

--- main.go
+++ main.go
@@ -14,8 +14,7 @@ import (
 func main() {
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
-		log.Print(err)
-		cacheDir = ""
+		log.Fatal(err)
 	}
 	cacheFile := path.Join(cacheDir, "bwmenu_names_cache")
 
